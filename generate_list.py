a58ff5a1fd13986895195432bd92965021e62fc6
Guillaume Gomez <guillaume.gomez@huawei.com>
2022-03-30 20:50:12 +0200

    Ensure the arch order to be predictable and generate general LLVM intrinsics

--- generate_list.py
+++ generate_list.py
@@ -3,6 +3,15 @@
 import os
 import json
 
+
+def append_translation(json_data, p, array):
+  it = json_data["index"][p]
+  content = it["docs"].split('`')
+  if len(content) != 5:
+    return
+  array.append('"{}" => "{}",'.format(content[1], content[3]))
+
+
 # First we generate the JSON version of the docs.
 os.system("cargo rustdoc -- -Zunstable-options --output-format json")
 
@@ -13,6 +22,7 @@ json_data = json.loads(content)
 # doc for the json is here:
 # https://doc.rust-lang.org/stable/nightly-rustc/rustdoc_json_types/index.html
 outputs = {}
+general = []
 ARCHS = [
     "AMDGPU",
     "aarch64",
@@ -28,22 +38,36 @@ ARCHS = [
 ]
 for p in json_data["paths"]:
   it = json_data["paths"][p]
-  # If we want, we can switch "x86" with another arch without problem.
-  if len(it["path"]) != 3 or it["path"][1] not in ARCHS:
+  if it["crate_id"] != 0:
+    # This is from an external crate.
+    continue
+  if it["kind"] != "function":
+    # We're only looking for functions.
+    continue
+  if len(it["path"]) == 2:
+    # This is a "general" intrinsic, not bound to a specific arch.
+    append_translation(json_data, p, general)
+    continue
+  elif len(it["path"]) != 3 or it["path"][1] not in ARCHS:
     continue
   arch = it["path"][1]
   if arch not in outputs:
     outputs[arch] = []
-  it = json_data["index"][p]
-  content = it["docs"].split('`')
-  if len(content) != 5:
-    continue
-  outputs[arch].append('"{}" => "{}",'.format(content[1], content[3]))
+  append_translation(json_data, p, outputs[arch])
+
+# Just in case:
+ARCHS.sort()
 
 print('match name {')
-for arch in outputs:
+for arch in ARCHS:
+   if arch not in outputs or len(outputs[arch]) == 0:
+     continue
    outputs[arch].sort()
    print('    // {}'.format(arch))
    print('\n'.join(['    {}'.format(x) for x in outputs[arch]]))
-print('_ => unimplemented!("***** unsupported LLVM intrinsic {}", name),')
+if len(general) > 0:
+  general.sort()
+  print('    // Intrinsics not bound to a specific arch')
+  print('\n'.join(['    {}'.format(x) for x in general]))
+print('    _ => unimplemented!("***** unsupported LLVM intrinsic {}", name),')
 print('}')
